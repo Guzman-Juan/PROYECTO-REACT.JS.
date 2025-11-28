08cc03941a9d2f4438bbc27853d63930c9d89ce7
Guzman Juan <gonzalezguzjuan@gmail.com>
2025-11-27 21:58:30 -0300

    paginado

--- src/Pages/ProductoDetalle.jsx
+++ src/Pages/ProductoDetalle.jsx
@@ -1,20 +1,39 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import TarjetaDetalle from "../components/TarjetaDetalle";  
+
 const ProductoDetalle = () => {
-  
   const { id } = useParams();
   const [producto, setProducto] = useState(null);
+const {cargando, setCargando} = useState (true);
 
   useEffect(() => {
-    fetch(`https://fakestoreapi.com/products/${id}`)
-      .then(respuesta => respuesta.json())
-      .then(dato => setProducto(dato));
-  },[id]);
+    const cargarProducto = async () => {
+      try { 
+        const respuesta = await fetch(`https://691e624bbb52a1db22bdbdc0.mockapi.io/proyecto-001/productos/${id}`);
+        if (!respuesta.ok) {
+          console.log('Error al obtener el producto');
+          setProducto(null);
+          return;
+        }
+        const dato = await respuesta.json();
+        setProducto(dato);
+      } catch (error) {
+        console.log('Error de red:', error);
+        setProducto(null);
+      } finally {
+        setCargando(false);
+      } 
+    };
+    cargarProducto();
+  }, [id]);
+  if (cargando) {
+    return <p>Cargando ......</p>;
+  } 
 
   if(!producto)
     return <p>Cargando ......</p>
-  
+
   return(
     <>
     <div className="container">
@@ -25,7 +44,6 @@ const ProductoDetalle = () => {
       </div>
     </div>
       
-
     </>
     
   );
